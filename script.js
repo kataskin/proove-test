d491d2c3486b9b26da56f3d3e794838809ae2e47
Maria Novokovich <kataskin@yandex.ru>
2016-07-05 13:12:41 +0300

    Валидация формы

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+//validation form
 
+var buttonLogIn = document.querySelector("button");
+var inputLogin = document.querySelector("input[type=text]");
+var inputPassword = document.querySelector("input[type=password]");
+
+var formIsValid = function() {
+  if (inputLogin.value === "" || inputPassword.value === "") {
+    buttonLogIn.setAttribute("disabled", "disabled");
+  } else {
+    buttonLogIn.removeAttribute("disabled");
+  }
+};
+
+window.onload = formIsValid;
+inputLogin.oninput = formIsValid;
+inputPassword.oninput = formIsValid;
